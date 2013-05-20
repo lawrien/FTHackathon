6d120bbb69999ed72d97f91d77656753df864081
Peter O'Shaughnessy <p_os@yahoo.co.uk>
2013-05-20 17:21:20 +0100

    More work on temp demo

--- temp/js/fthack.js
+++ temp/js/fthack.js
@@ -1,5 +1,7 @@
 $(function() {
 
+    var chosenParaIndexes = [0,1,2];
+
     $('#ftHackShare').click(function() {
 
         share();
@@ -8,8 +10,78 @@ $(function() {
 
     function share() {
 
-        $('#ftHackSharePanel').show();
+        updateSharePanel();
+
+        updateShareButtons();
+
+        updateFades();
+
+        $('.container .editorialSection p').mouseover(function() {
+
+            var paraIndex = $('#storyContent').find('p').index(this);
+
+            chosenParaIndexes = [paraIndex, paraIndex+1, paraIndex+2];
+
+            updateSharePanel();
+            updateFades();
+
+        });
+
+        $($sharePanelEl).show();
+
         $('#ftHackDefaultPanel').hide();
+
+    }
+
+    function updateSharePanel() {
+
+        var title = document.title;
+        var subtitle = $('.standfirst').length > 0 ? $('.standfirst')[0] : $('.byline span')[0];
+
+        var parasHtml = '';
+
+        for( var i=0; i < chosenParaIndexes.length; i++ ) {
+
+            var paraIndex = chosenParaIndexes[i];
+            parasHtml += '<p>' + $('#storyContent p:eq('+ paraIndex + ')').html() + '</p>';
+
+        }
+
+        $sharePanelEl = $('#ftHackSharePanel');
+        $contentsEl = $('#ftHackSharePanel .contents');
+
+        $('.title', $contentsEl).html(document.title);
+
+        $('.subtitle', $contentsEl).html(subtitle);
+
+        $('.paras', $contentsEl).html(parasHtml);
+
+    }
+
+    function updateShareButtons() {
+
+        $('.twitter-share-button', $sharePanelEl).attr('href', 'https://twitter.com/share?count=none&text='+' original: ');
+
+        setupTwitterButton();
+
+    }
+
+    function setupTwitterButton() {
+
+        !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+
+    }
+
+    function updateFades() {
+
+        $('#storyContent p').addClass('fade');
+
+        for( var i=0; i < chosenParaIndexes.length; i++ ) {
+
+            var paraIndex = chosenParaIndexes[i];
+            $('#storyContent p:eq('+ paraIndex + ')').removeClass('fade');
+
+        }
 
     }
 
