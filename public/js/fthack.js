81bea8d4622895edb4d8bb124a1a745b2009e35b
Peter O'Shaughnessy <p_os@yahoo.co.uk>
2013-05-20 19:57:57 +0100

    Fix Twitter share

--- public/js/fthack.js
+++ public/js/fthack.js
@@ -1,141 +1,142 @@
-$(function() {
+var shareLink = '';
+var chosenParaIndexes = [0,1,2];
 
-    var shareLink = '';
-    var chosenParaIndexes = [0,1,2];
+function init() {
 
-    function init() {
+    $('#ftHackShare').click(function() {
 
-        $('#ftHackShare').click(function() {
+        share();
 
-            share();
+    });
 
-        });
+    $('#ftHackSharePanel .twitter-share').click(function() {
 
-        $('#ftHackSharePanel .twitter-share').click(function() {
+        fetchSharableLink();
 
-            fetchSharableLink();
+    });
 
-        });
+}
 
-    }
-    init();
+function share() {
 
-    function share() {
+    updateSharePanel();
+
+    updateFades();
+
+    $('.container .editorialSection p').mouseover(function() {
+
+        var paraIndex = $('#storyContent').find('p').index(this);
+
+        chosenParaIndexes = [paraIndex, paraIndex+1, paraIndex+2];
 
         updateSharePanel();
-
         updateFades();
 
-        $('.container .editorialSection p').mouseover(function() {
+    });
 
-            var paraIndex = $('#storyContent').find('p').index(this);
+    $($sharePanelEl).show();
 
-            chosenParaIndexes = [paraIndex, paraIndex+1, paraIndex+2];
+    $('#ftHackDefaultPanel').hide();
 
-            updateSharePanel();
-            updateFades();
+}
 
-        });
+function updateSharePanel() {
 
-        $($sharePanelEl).show();
+    var title = document.title;
+    var subtitle = $('.standfirst').length > 0 ? $('.standfirst')[0] : $('.byline span')[0];
 
-        $('#ftHackDefaultPanel').hide();
+    var parasHtml = '';
+
+    for( var i=0; i < chosenParaIndexes.length; i++ ) {
+
+        var paraIndex = chosenParaIndexes[i];
+        parasHtml += '<p>' + $('#storyContent p:eq('+ paraIndex + ')').html() + '</p>';
 
     }
 
-    function updateSharePanel() {
+    $sharePanelEl = $('#ftHackSharePanel');
+    $contentsEl = $('#ftHackSharePanel .contents');
 
-        var title = document.title;
-        var subtitle = $('.standfirst').length > 0 ? $('.standfirst')[0] : $('.byline span')[0];
+    $('.title', $contentsEl).html(document.title);
 
-        var parasHtml = '';
+    $('.subtitle', $contentsEl).html(subtitle);
 
-        for( var i=0; i < chosenParaIndexes.length; i++ ) {
+    $('.paras', $contentsEl).html(parasHtml);
 
-            var paraIndex = chosenParaIndexes[i];
-            parasHtml += '<p>' + $('#storyContent p:eq('+ paraIndex + ')').html() + '</p>';
+}
 
+/*
+function updateShareButtons() {
+
+    $('.twitter-share-button', $sharePanelEl).attr('href', 'https://twitter.com/share?count=none&text='+' original: ');
+
+    setupTwitterButton();
+
+}
+
+function setupTwitterButton() {
+
+    !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+
+}
+*/
+
+function updateFades() {
+
+    $('#storyContent p').addClass('fade');
+
+    for( var i=0; i < chosenParaIndexes.length; i++ ) {
+
+        var paraIndex = chosenParaIndexes[i];
+        $('#storyContent p:eq('+ paraIndex + ')').removeClass('fade');
+
+    }
+
+}
+
+function fetchSharableLink() {
+
+    var dataValues = {
+        'articleId': articleUUID,
+        'paras': chosenParaIndexes
+    };
+
+    console.log('data values', dataValues);
+
+    $.ajax({
+        url: '/shift/share',
+        type: 'post',
+        data: JSON.stringify(dataValues),
+        success: function(data) {
+
+            console.log('data', data);
+
+            shareLink = 'http://localhost:9292/shift/shared/' + data;
+            twitterShare();
+
+        },
+        error: function() {
+            console.log('Error');
         }
+    });
 
-        $sharePanelEl = $('#ftHackSharePanel');
-        $contentsEl = $('#ftHackSharePanel .contents');
+}
 
-        $('.title', $contentsEl).html(document.title);
+function twitterShare() {
 
-        $('.subtitle', $contentsEl).html(subtitle);
+    var width = 650,
+        height = 450,
+        left = Math.round( screen.width / 2 - width / 2 ),
+        top = Math.round( screen.height / 2 - height / 2 );
 
-        $('.paras', $contentsEl).html(parasHtml);
+    var text = document.title + ' ' + shareLink;
 
-    }
+    window.open('https://twitter.com/intent/tweet?original_referer=&text='+text, // URL
+        undefined, // window name
+        'width='+width+',height='+height+',left='+left+',top='+top ); // window features
 
-    /*
-    function updateShareButtons() {
+}
 
-        $('.twitter-share-button', $sharePanelEl).attr('href', 'https://twitter.com/share?count=none&text='+' original: ');
-
-        setupTwitterButton();
-
-    }
-
-    function setupTwitterButton() {
-
-        !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
-
-    }
-    */
-
-    function updateFades() {
-
-        $('#storyContent p').addClass('fade');
-
-        for( var i=0; i < chosenParaIndexes.length; i++ ) {
-
-            var paraIndex = chosenParaIndexes[i];
-            $('#storyContent p:eq('+ paraIndex + ')').removeClass('fade');
-
-        }
-
-    }
-
-    function fetchSharableLink() {
-
-        var dataValues = {
-            'articleId': articleUUID,
-            'paras': chosenParaIndexes
-        };
-
-        $.ajax({
-            url: '/share',
-            type: 'post',
-            data: dataValues,
-            success: function(data) {
-
-                console.log('data', data);
-
-                shareLink = data;
-                twitterShare();
-
-            },
-            error: function() {
-                console.log('Error');
-            }
-        });
-
-    }
-
-    function twitterShare() {
-
-        var width = 650,
-            height = 450,
-            left = Math.round( screen.width / 2 - width / 2 ),
-            top = Math.round( screen.height / 2 - height / 2 );
-
-        var text = document.title + ' ' + shareLink;
-
-        window.open('https://twitter.com/intent/tweet?original_referer=&text='+text, // URL
-            undefined, // window name
-            'width='+width+',height='+height+',left='+left+',top='+top ); // window features
-
-    }
-
+$(function() {
+    init();
 });
