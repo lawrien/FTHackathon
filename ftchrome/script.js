b514f70b9e51f1d8b664e40ef515f014c0a6e191
Lawrie Nichols <lawrie.nichols@pearson.com>
2013-05-21 11:55:25 +0100

    Chrome extension

--- ftchrome/script.js
+++
@@ -1,7 +0,0 @@
-// Test the text of the body element against our regular expression.
-if (document.body.innerText) {
-  // The regular expression produced a match, so notify the background page.
-  console.log('test');
-} else {
-  // No match was found.
-}
